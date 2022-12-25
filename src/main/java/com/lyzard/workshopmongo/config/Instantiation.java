61fc01d0d9bcdf3d53638a8ad48535b1fdb71d6a
Thiago C. Santos <thiagocarretero433@gmail.com>
2022-12-25 15:04:40 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/lyzard/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lyzard/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.lyzard.workshopmongo.domain.Post;
 import com.lyzard.workshopmongo.domain.User;
+import com.lyzard.workshopmongo.dto.AuthorDTO;
 import com.lyzard.workshopmongo.repository.PostRepository;
 import com.lyzard.workshopmongo.repository.UserRepository;
 
@@ -35,9 +36,8 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
-				maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
