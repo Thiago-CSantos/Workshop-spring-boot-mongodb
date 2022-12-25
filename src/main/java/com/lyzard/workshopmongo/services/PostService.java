b2c7e5682a01d78a3bafa26ab763e2a017fe3c73
Thiago C. Santos <thiagocarretero433@gmail.com>
2022-12-25 15:22:18 -0300

    Obtendo um post por id

---
+++ src/main/java/com/lyzard/workshopmongo/services/PostService.java
@@ -0,0 +1,19 @@
+package com.lyzard.workshopmongo.services;
+
+import org.apache.el.stream.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.lyzard.workshopmongo.domain.Post;
+import com.lyzard.workshopmongo.repository.PostRepository;
+
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
