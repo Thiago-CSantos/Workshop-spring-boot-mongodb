fbe8c21caf18fb0a0d8f815f295c93e4949828dc
Thiago C. Santos <thiagocarretero433@gmail.com>
2022-12-25 15:45:17 -0300

    Consulta simples com @query

--- src/main/java/com/lyzard/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lyzard/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.lyzard.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lyzard.workshopmongo.domain.Post;
@@ -10,4 +11,7 @@ import com.lyzard.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
