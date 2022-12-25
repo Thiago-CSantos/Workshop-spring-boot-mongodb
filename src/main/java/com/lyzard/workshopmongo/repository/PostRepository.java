43d053808a09f39976d1e146fbc7e1202762c642
Thiago C. Santos <thiagocarretero433@gmail.com>
2022-12-25 15:48:46 -0300

    Consulta com varios criterios

--- src/main/java/com/lyzard/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lyzard/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.lyzard.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -11,7 +12,10 @@ import com.lyzard.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	List<Post> findByTitleContainingIgnoreCase(String text);
-	
+
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
+
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/lyzard/workshopmongo/services/PostService.java
+++ src/main/java/com/lyzard/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.lyzard.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.apache.el.stream.Optional;
@@ -20,6 +21,11 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 	
 }
