633f3f47990e4e73aa3d4a7de3121e764a076f0f
Thiago C. Santos <thiagocarretero433@gmail.com>
2022-12-25 15:26:27 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/lyzard/workshopmongo/domain/Post.java
+++ src/main/java/com/lyzard/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.lyzard.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.lyzard.workshopmongo.dto.AuthorDTO;
+import com.lyzard.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -71,6 +76,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
